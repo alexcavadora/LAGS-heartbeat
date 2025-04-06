a4b7e103a095e1bf1cab692e6783c37b58ea7476
Isaac-Salas <iscprepa@gmail.com>
2025-04-06 17:02:39 -0600

    EnemieSoundManager

---
+++ Enemies/enemie_sound.gd
@@ -0,0 +1,23 @@
+extends Node3D
+class_name EnemieSound
+const ENEMIE_ATTACK = preload("res://assets/SFX/Enemie Attack.mp3")
+const ENEMIE_STUN = preload("res://assets/SFX/Enemie stun.mp3")
+const ENEMY_DEATH = preload("res://assets/SFX/Enemy Death.mp3")
+
+@onready var currentsound
+@onready var audios : AudioStreamPlayer3D = $AudioStreamPlayer3D
+
+func play_sound(sound):
+	audios.play(sound)
+
+
+func _on_health_dead():
+	play_sound(ENEMY_DEATH)
+
+
+func _on_health_just_hit(health):
+	play_sound(ENEMIE_STUN)
+
+
+func _on_attacker_attacked():
+	pass # Replace with function body.
